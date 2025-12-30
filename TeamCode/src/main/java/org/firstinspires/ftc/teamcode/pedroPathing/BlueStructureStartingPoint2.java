9efcc3d8c40cd7e28719c22f95ae54a6d67c1609
William-D4095 <bobhmazilla@gmail.com>
2025-12-30 09:33:27 -0500

    update version #

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/BlueStructureStartingPoint2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/BlueStructureStartingPoint2.java
@@ -148,7 +148,7 @@ public class BlueStructureStartingPoint2 extends OpMode {
 
     @Override
     public void loop() {
-        telemetry.addLine("7 POOPS ON EILEEN"); // ------------ VERY IMPORTANT VERSION NUMBER LINE -----------
+        telemetry.addLine("8 POOPS ON EILEEN"); // ------------ VERY IMPORTANT VERSION NUMBER LINE -----------
         follower.update();
         updateStateMachine();
     }
