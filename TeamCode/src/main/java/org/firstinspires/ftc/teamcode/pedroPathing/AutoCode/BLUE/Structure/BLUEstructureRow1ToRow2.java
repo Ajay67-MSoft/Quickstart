143168619f681d559f01ce3bc3ea259175262bd9
William-D4095 <bobhmazilla@gmail.com>
2026-03-06 20:52:39 -0500

    temp commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/AutoCode/BLUE/Structure/BLUEstructureRow1ToRow2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/AutoCode/BLUE/Structure/BLUEstructureRow1ToRow2.java
@@ -102,7 +102,7 @@ public class BLUEstructureRow1ToRow2 extends OpMode {
     private final Pose poseCollectsRow1 = new Pose(15, 80, Math.toRadians(180));
     private final Pose poseShootRow1 = new Pose(55, 100, Math.toRadians(140)); // increase x lower y to move farther from goal
     private final Pose poseToCollect2 = new Pose(44.4, 60, Math.toRadians(180));
-    private final Pose poseCollectsRow2 = new Pose(15, 56, Math.toRadians(180));
+    private final Pose poseCollectsRow2 = new Pose(10, 56, Math.toRadians(180));
     private final Pose posePreparationPositionToMoveToShootPos = new Pose (44.4, 56, Math.toRadians(180));
     private final Pose poseShootRow2 = new Pose(55, 100, Math.toRadians(142)); // increase x lower y to move farther from goal
     private final Pose poseEnd = new Pose(24, 68, Math.toRadians(180));
